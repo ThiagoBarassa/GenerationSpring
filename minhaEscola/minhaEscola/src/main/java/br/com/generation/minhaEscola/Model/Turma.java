c9d1d062a3bc41aec430e99204fe35de71e87fb2
Thiago Barassa <thigobarassa@gmail.com>
2020-12-08 15:15:25 -0300

    Exercicio Minha Escola

---
+++ minhaEscola/minhaEscola/src/main/java/br/com/generation/minhaEscola/Model/Turma.java
@@ -0,0 +1,49 @@
+package br.com.generation.minhaEscola.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "turma")
+public class Turma {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=5, max=25)
+	private String turma;
+	
+	boolean ativo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTurma() {
+		return turma;
+	}
+
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+}
